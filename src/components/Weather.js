39daaf651aff0c9e65794a28ba3d5443a81bfa97
Riya3618 <riyajhagogo@gmail.com>
2025-02-16 19:02:26 +0530

    `Refactored Weather.js component and styles.css file with minor changes and improvements.`

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,38 +1,48 @@
-import moment from 'moment/moment';
 import React from 'react';
-import './styles.css';
+import moment from 'moment';
 import { Button } from 'semantic-ui-react';
+import './styles.css'; 
 
-const refresh = () => {
-    window.location.reload();
-  }
-const WeatherCard=({weatherData})=>{
+const WeatherCard = ({ weatherData }) => {
   if (weatherData.cod !== 200) {
     return ;
-   }
-   return(
+  }
+
+  const refresh = () => {
+    window.location.reload();
+  };
+
+  return (
     <div className="main">
       <div className="top">
         <p className="header">{weatherData.name}</p>
         <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
       </div>
-      <div className="flex">
-        <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
-        <p className="description"> {weatherData.weather && weatherData.weather.length > 0 ? weatherData.weather[0].main : 'No data available'}</p>
+      <div className="flex day-description">
+        <p className="day">
+          {moment().format('dddd')}, <span>{moment().format('LL')}</span>
+        </p>
+        {weatherData.weather && weatherData.weather.length > 0 && (
+          <span className="description">
+            <img
+              src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+              alt={weatherData.weather[0].description}
+              className="weather-icon"
+            />
+           {weatherData.weather[0].main}
+          </span>
+        )}
       </div>
-
       <div className="flex">
-        <p className="temp">Temperature: {weatherData.main? `${weatherData.main.temp} °C` : 'No data available'}</p>
+        <p className="temp">Temperature: {weatherData.main ? `${weatherData.main.temp} °C` : 'No data available'}</p>
         <p className="temp">Humidity: {weatherData.main ? `${weatherData.main.humidity} %` : 'No data available'}</p>
       </div>
-
       <div className="flex">
         <p className="sunrise-sunset">Sunrise: {weatherData.sys && weatherData.sys.sunrise ? new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN') : 'No data available'}</p>
         <p className="sunrise-sunset">Sunset: {weatherData.sys && weatherData.sys.sunset ? new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN') : 'No data available'}</p>
       </div>
-    
-  </div>
-   );
-}
-export default WeatherCard;
+    </div>
+  );
+};
 
+export default WeatherCard;
